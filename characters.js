b749559b3eaa6e9c44fd525d8cc1b130b1a65719
Adit Limbasia <127438395+Aditpj@users.noreply.github.com>
2025-01-27 15:24:17 -0800

    Update characters.js

--- characters.js
+++ characters.js
@@ -14,25 +14,34 @@ class Tank {
         //this.healthBar = new HealthBar(this);
 
         // Update this when we get character sprites
-        this.spritesheet = ASSET_MANAGER.getAsset("./Spritetemp.PNG");
+        this.spritesheet = ASSET_MANAGER.getAsset("./Megaman.PNG");
         this.animations = [];
         this.loadAnimations();
 
     };
 
     loadAnimations() {
-        for (var i = 0; i < 2; i++) { //State
-            this.animations.push([]);
-            for (var j = 0; j < 4; j++) { //Direction Facing
-                this.animations[i].push([]);
-            }
-        }
+        // for (var i = 0; i < 2; i++) { //State
+        //     this.animations.push([]);
+        //     for (var j = 0; j < 4; j++) { //Direction Facing
+        //         this.animations[i].push([]);
+        //     }
+        // }
 
         //Idle animation for state 0
         //Facing Left = 0
         //this.animations[0][0] = new Animator(this.spritesheet, 0, 0, 105, 110, 1, .2, 30, false, true);
-        this.animations[0][0] = new Animator(ASSET_MANAGER.getAsset("./Megaman sprite.png"), 0, 0, 35, 30, 4, 0.4, 0, false, true);
-
+        //this.animations[0] = new Animator(ASSET_MANAGER.getAsset("./Megaman sprite.png"), 0, 0, 35, 30, 4, 0.4, 0, false, true);
+        
+            console.log("Loading animation...");
+        
+            this.animations = new Animator(
+                ASSET_MANAGER.getAsset("./Megaman sprite.png"), 
+                0, 0, 35, 30, 4, 0.4, 0, false, true
+            );
+        
+            console.log("Animation assigned:", this.animations);
+        
 
 
     };
@@ -42,10 +51,23 @@ class Tank {
     };
 
     draw(ctx) {
-
-        this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, PARAMS.SCALE);
+        // ctx.fillStyle = "red";
+        // ctx.fillRect(this.x, this.y, 35, 30);
+        // this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, PARAMS.SCALE);
         //this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    
+            // Debug: Draw a red rectangle where the tank should be
+            ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, 35, 30);
 
+    // Manually draw sprite for debugging
+    const sprite = ASSET_MANAGER.getAsset("./Megaman sprite.png");
+    if (sprite) {
+        console.log("Manually drawing sprite...");
+        ctx.drawImage(sprite, 0, 0, 35, 30, this.x, this.y, 35, 30);
+    } else {
+        console.error("Sprite is missing!");
+    }
 
         //this.healthBar.draw(ctx);
     };
