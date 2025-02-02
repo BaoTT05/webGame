56b730b4af75d789c2487577f3a5c34464988b18
Tyler Nguyen <tylernguyen360@yahoo.com>
2025-02-02 15:36:46 -0800

    Fixed the animations

--- characters.js
+++ characters.js
@@ -1,71 +1,73 @@
 // characters.js (Tank character)
 class Tank {
     constructor(game, x, y) {
-      Object.assign(this, { game, x, y });
-      this.location = { x, y };
-      this.speed = 1;
-      this.facing = 0; // 0 = left, 1 = right, etc.
-      this.state = 0; // 0 = idle, 1 = walking
-  
-      this.currentHealth = 100;
-      this.maxHealth = 100;
-  
-      // Load the spritesheet using the AssetManager
-      this.spritesheet = ASSET_MANAGER.getAsset("./Megaman sprite.png");
-      this.animations = [];
-      this.loadAnimations(this.spritesheet);
-    }
-  
-    loadAnimations(spritesheet) {
-      // For simplicity, we handle two states (idle, walking) and two directions (left/right)
-      for (var i = 0; i < 2; i++) {
-        this.animations.push([]);
-        for (var j = 0; j < 2; j++) {
-          this.animations[i].push(null);
+        Object.assign(this, { game, x, y });
+        this.location = { x, y };
+        this.speed = 1;
+        this.facing = 0; // 0 = left, 1 = right, etc.
+        this.state = 0; // 0 = idle, 1 = walking
+    
+        this.currentHealth = 100;
+        this.maxHealth = 100;
+    
+        // Load the spritesheet using the AssetManager
+        this.spritesheet = ASSET_MANAGER.getAsset("./Megaman sprite.png");
+        this.animations = [];
+        this.loadAnimations(this.spritesheet);
         }
-      }
-  
-      // Idle animations:
-      // Left-facing idle animation
-      this.animations[0][0] = new Animator(spritesheet, 70, 61, 35, 30, 2, 0.4, 0, false, true);
-      // Right-facing idle animation
-      this.animations[0][1] = new Animator(spritesheet, 0, 61, 35, 30, 2, 0.4, 0, false, true);
-  
-      // Walking animations:
-      // Left-facing walking animation
-      this.animations[1][0] = new Animator(spritesheet, 70, 61, 35, 30, 4, 0.4, 0, false, true);
-      // Right-facing walking animation
-      this.animations[1][1] = new Animator(spritesheet, 0, 0, 35, 30, 4, 0.4, 0, false, true);
-    }
-  
+    
+        loadAnimations(spritesheet) {
+        // For simplicity, we handle two states (idle, walking) and two directions (left/right)
+        for (var i = 0; i < 2; i++) {
+            this.animations.push([]);
+            for (var j = 0; j < 2; j++) {
+            this.animations[i].push(null);
+            }
+        }
+    
+        // Idle animations:
+        // Left-facing idle animation
+        this.animations[0][0] = new Animator(spritesheet, 70, 61, 35, 30, 2, 0.4, 0, false, true);
+        // Right-facing idle animation
+        this.animations[0][1] = new Animator(spritesheet, 0, 61, 32, 30, 2, 0.4, 0, false, true);
+    
+        // Walking animations:
+        // Left-facing walking animation
+        this.animations[1][0] = new Animator(spritesheet, 0, 31, 35, 30, 4, 0.4, 0, false, true);
+        // Right-facing walking animation
+        this.animations[1][1] = new Animator(spritesheet, 0, 0, 35, 30, 4, 0.4, 0, false, true);
+        }
+
     update() {
-      let moving = false;
-      if (this.game.keys.left && !this.game.keys.right) {
-        this.state = 1;
-        this.facing = 0;
-        moving = true;
-      } else if (!this.game.keys.left && this.game.keys.right) {
-        this.state = 1;
-        this.facing = 1;
-        moving = true;
-      }
-      if (!moving) {
-        this.state = 0;
-      }
-      // You can add additional logging or state changes as needed.
-    }
-  
+        let moving = false;
+        if (this.game.keys.left && !this.game.keys.right) {
+            this.state = 1;
+            this.facing = 0;
+            moving = true;
+        } else if (!this.game.keys.left && this.game.keys.right) {
+            this.state = 1;
+            this.facing = 1;
+            moving = true;
+        }
+        if (!moving) {
+            this.state = 0;
+        }
+        // You can add additional logging or state changes as needed.
+        }
+
     draw(ctx) {
       // For debugging, draw a red rectangle showing the Tank's position.
-      ctx.fillStyle = "red";
-      ctx.fillRect(this.x, this.y, 35, 30);
-  
-      // Draw the animation sprite with a scale factor of 1.
-      if (this.animations[this.state] && this.animations[this.state][this.facing]) {
-        this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x, this.y, 1);
-      } else {
-        console.error("Missing animation for state:", this.state, "facing:", this.facing);
-      }
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x, this.y, 35, 30);
+    
+        // Draw the animation sprite with a scale factor of 1.
+        if (this.animations[this.state] && this.animations[this.state][this.facing]) {
+            this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x, this.y, 1);
+            console.log("Animations:", this.animations);
+console.log("State:", this.state, "Facing:", this.facing);
+console.log("Current Animation:", this.animations[this.state]?.[this.facing]);
+        } else {
+            console.error("Missing animation for state:", this.state, "facing:", this.facing);
+        }
+        }
     }
-  }
-  
